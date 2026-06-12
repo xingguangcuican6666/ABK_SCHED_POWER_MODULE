50430972079b177562a8d06304aaace4c4dadc89
xingguangcuican <xingguangcuican666@foxmail.com>
2026-06-12 06:45:17 +0000

    Remove deprecated Kotlin Android Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
     id("com.android.application") version "9.1.1" apply false
-    kotlin("android") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
